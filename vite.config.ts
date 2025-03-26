a7a80a183983f6721de8e8d89f019ad02fec83a9
davidbadelllab <davidbadelljose5@gmail.com>
2025-03-26 18:36:09 -0300

    Aumentado chunkSizeWarningLimit a 2000kb en vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 2000, // Aumenta el límite a 2000kb
+  },
   assetsInclude: ['**/*.glb']
 })
